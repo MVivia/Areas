8844abe2f7a0f76fd63d2822f3f38441dd5c3d52
MVivia <hernandezmondragonnormaviviana1cpv@cetis50cdmx.com>
2024-03-05 19:43:34 -0600

    Se sube practica de areas

---
+++ script.js
@@ -0,0 +1,27 @@
+function Areac(){
+    let num1 =Number(document.getElementById ('n1').value);
+    let respuesta1= num1 * num1 ;
+    document.getElementById('respuesta1').value = respuesta1;
+}
+
+function Areaci(){
+    let num1 =Number(document.getElementById ('n1').value);
+    let respuesta2= num1^2 * 3.1416;
+    document.getElementById('respuesta2').value = respuesta2;
+    
+}
+
+function Arear(){
+    let num1 =Number(document.getElementById ('n1').value);
+    let num2 =Number(document.getElementById ('n2').value);
+    let respuesta3= num1 * num2;
+    document.getElementById('respuesta3').value = respuesta3;
+    
+}
+function Areat(){
+    
+    let num1 =Number(document.getElementById ('n1').value);
+    let num2 =Number(document.getElementById ('n2').value);
+        let respuesta4 = num1 * num2 /2;
+        document.getElementById('respuesta4').value = respuesta4;
+    }
